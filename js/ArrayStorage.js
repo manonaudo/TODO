d8e9585c23d0504b7feadf9d50f302a7462ba9bc
manonaudo <89037592+manonaudo@users.noreply.github.com>
2021-08-16 19:57:22 +0200

    Update ArrayStorage.js

--- js/ArrayStorage.js
+++ js/ArrayStorage.js
@@ -1,13 +1,13 @@
 'use strict';
 
-//Stockage local de nos taches sous forma tableau
+//Stockage local de nos taches sous forme de tableau
 class ArrayStorage {
   
-  //Un construsteur pour initialiser l'objet avec le nom de la clé et son contenu (valeur)
+  //Un construsteur pour initialiser l'objet avec le nom de la clé et sa valeur
   constructor(name) {
-    this.name = name; //pourrait etre task
+    this.name = name; 
 
-    this.list = this.get(); //pourrait etre ['tourner des tutos']
+    this.list = this.get(); 
   } 
 
 
@@ -17,30 +17,24 @@ class ArrayStorage {
     if (!localStorage.getItem(this.name)) {
       // retourne le booleen contraire a celui qui aurait du etre renvoyé
       // si il n'y a pas cette clé, on rentre dans la condition 
-      // la personne n'a pas de clé, n'a pas de taches.
       localStorage.setItem(this.name, '[]'); // Ajoute la clé avec sa valeur string contenant un tableau
     }
-
     return JSON.parse(localStorage.getItem(this.name));
   }
 
    // Une methode pour ajouter une valeur dans le tableau
   set(value) {
-    //met a jour la propriété list (le stockage + ce que l'utilisateur ajoute)
-    //list pour tableau, methode push pour ajouter a la fin.
+    //met à jour la propriété list (le stockage + ce que l'utilisateur ajoute)
     this.list.push(value); // ajoute une valeur a la fin de mon tableau
-    //mettre a jour la clé sur le serveur local : JSON.stringify permet de transformer notre propriété liste qui est un tableau, en string
-
+    //met à jour la clé sur le serveur local : JSON.stringify permet de transformer la propriété liste qui est un tableau, en string
     localStorage.setItem(this.name, JSON.stringify(this.list));
   } 
 
   //Une methode pour supprimer une valeur du tableau (de la liste et du stockagelocal)
   remove(value) {
     const index = this.list.indexOf(value); // indique l'indice du tableau this.list qui a la valeur "value"
-
     this.list.splice(index, 1); //Splice : supprime un ou plusieurs element du tableau this.list. splice(indice a suppr, combien d'indice a suppr)
-    //mettre a jour et supprime l'element supprimé
-
+    //met à jour et supprime l'element
     localStorage.setItem(this.name, JSON.stringify(this.list));
   } 
 
