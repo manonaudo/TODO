69a5732b8cfedb8ac4daf14a0b4bf20f596e7e0c
Manon AUDO <89037592+manonaudo@users.noreply.github.com>
2021-08-16 20:29:36 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,38 +6,24 @@ const add = document.getElementById('add');
 const clear = document.getElementById('clear');
 const url = document.getElementById('url');
 const load = document.getElementById('load');
-
- // On recupere la class ArrayStorage de arrayStorage.js
 const storage = new ArrayStorage('tasks');
-
-// task : la tache à afficher dans la page html
 const tasks = storage.list;
 
-function taskToDOM(task) // Fonction qui ajoute les taches au dom avec un bouton de suppression auquel on ajoute un evenement
+function taskToDOM(task)
 {
-  if (typeof task === 'string' && task != false) // si chaine non vide
+  if (typeof task === 'string' && task != false)
     {
-      //Creer un element <li> et un <button> dans notre page html
       const li = document.createElement('li');
       const remove = document.createElement('button'); 
-	    
-	//On ajoute du texte a li et a button 
       li.textContent = task;
-      remove.textContent = 'REMOVE'; // Donc dans li il y a le texte contenu dans tasks[i] et le bouton
-
+      remove.textContent = 'REMOVE';
       remove.addEventListener('click', () => {
-        // recupere le texte qui est associé à la tache en appyuant sur son bouton remove
-        const value = remove.parentNode.firstChild.textContent; // bouton html remove. On remonte sur le parent (li). On recupere son 1er enfant (la tache). On recupère le contenu textuel de la tache.
-
-        storage.remove(value); // methode créée dans arrayStorage. On peut l'utiliser grace a la constante storage
-
-        list.removeChild(remove.parentNode); //Supprime le parent de remove donc li.
+        const value = remove.parentNode.firstChild.textContent; 
+        storage.remove(value);
+        list.removeChild(remove.parentNode);
       });
-	    
-      li.appendChild(remove); //li contient le texte de tasks[i] et le bouton 
-
-      list.insertBefore(li, list.firstChild); //On insert lans list les li en premier enfant a chaque boucle
-
+      li.appendChild(remove);
+      list.insertBefore(li, list.firstChild);
       return true;
     }
 
@@ -45,33 +31,28 @@ function taskToDOM(task) // Fonction qui ajoute les taches au dom avec un bouton
 } 
 
 
-tasks.forEach(task => taskToDOM(task)); // gere l'ajout de tache avec le bouton ADD et la touche 'Enter'
+tasks.forEach(task => taskToDOM(task));
 
-function newTask() {  //Se produit quand l'utilisateur click sur le bouton add
-  //Verifier que l'utilisateur ne rajoute pas une tache qu'il a dejà
+function newTask() { 
   if (storage.list.indexOf(input.value) === -1 && taskToDOM(input.value)) {
-    // si indexOf vaut -1 ca veut dire qu'il n'y a pas de doublon
     storage.set(input.value);
-    input.value = ''; // vide le champ de text 
+    input.value = ''; 
   }
-
-  input.focus(); // focus : le curseur reste dans le cadre 
+  input.focus();
 }
 
 
 add.addEventListener('click', newTask); 
 
-// On supprime la liste du DOM et du navigateur
 input.addEventListener('keydown', e => {
   if (e.key === 'Enter') {
     newTask();
   }
 }); 
 
-// Gere l'importation de taches 
 clear.addEventListener('click', () => {
   storage.clear();
-  list.innerHTML = ''; // On indique que notre liste n'a aucun contenu/ une chaine vide
+  list.innerHTML = '';
 }); 
 
 load.addEventListener('click', newArray);
@@ -84,21 +65,15 @@ url.addEventListener('keydown', e => {
 function newArray() {
   fetch(url.value)
   .then(response => {
-    //Une fois la promesse resolue, on recupere la reponse
     if (response.ok) {
-      //Verifie que la promesse soit tenue
       return response.json();
-    } //Si promesse pas tenue -> une erreur
-
+    }
     throw new Error(`${response.statusText} (${response.status})`);
     })
    .then(tasks => {
-    // on verifie si la reponse est bien un tableau au format json
      if (Array.isArray(tasks)) {
-      // Utilise l'objet Array avec la methode isArray() : Indique si tasks est un tableau
       tasks.forEach(task => {
         if (storage.list.indexOf(task) === -1 && taskToDOM(task)) {
-          // si indexof vaut -1 ca veut dire qu'il n'y a pas de doublon
           storage.set(task);
         }
       });
